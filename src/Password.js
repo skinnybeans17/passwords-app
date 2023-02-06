11267d7c3762d7ee9528b983157033a5de2b100c
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2023-02-06 14:57:53 -0800

    good

--- src/Password.js
+++ src/Password.js
@@ -1,23 +1,32 @@
 import { useState } from 'react'
 
-function random(n) {
-    return Math.floor(Math.random() * n)
-}
-
 function generatePassword() {
   // generate a password here
   console.log('generating password')
   const str = 'abcdefghijklmnopqrstuvwxyzQWERTYUIOPASDFGHJKLZXCVBNM1234567890';
-  const strLength = str[random(str.length)];
+  let strLength = "";
+  for (let i = 0; i < 8; i++) {
+    strLength += str.charAt(
+        Math.floor(Math.random() * str.length)
+    )};
   return strLength;
 }
 
 function Password() {
+  const [name, setName] = useState('')
   const [password, setPassword] = useState('p@$$w0rd')
 
   return (
   <div>
-    <div>{password}</div>
+    <input
+        placeholder="Enter Your Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}/>
+    <input 
+    type="text"
+    onChange={(e) => setPassword(e.target.value)}
+    value={password}
+    />
     <div>
       <button onClick={(e) => {
         setPassword(generatePassword())
