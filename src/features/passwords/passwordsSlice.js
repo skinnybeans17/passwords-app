f84e9f9022bf718a4b1e81ab9bf9ec2b608a87be
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2023-02-06 17:40:23 -0800

    complete

--- src/features/passwords/passwordsSlice.js
+++ src/features/passwords/passwordsSlice.js
@@ -1,5 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit' // 1
+
+// 2
 const initialState = {
-    value: [
-      { password: 'hello', name: 'test'}
-    ],
+  value: [{ password: 'hello', name: 'test'}],
 }
+
+// 3 Add the password slice
+export const passwordsSlice = createSlice({
+	name: 'passwords',
+	initialState,
+	reducers: {
+		addPassword: (state, action) => {
+			state.value.push(action.payload)
+		}
+	}
+})
+
+export const { addPassword } = passwordsSlice.actions
+export default passwordsSlice.reducer
